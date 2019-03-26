fff15acf55fae63aee7d6856d9107669f462a886
Peter Kings <p.kings@icloud.com>
2019-03-26 13:59:51 +0100

    restructure database connection module

--- src/ewiis3DatabaseConnector/__init__.py
+++ src/ewiis3DatabaseConnector/__init__.py
@@ -1,11 +1,11 @@
-from .ewiis3DatabaseConnector import hello_world
-from .database_connection import load_consumption_and_production_data  # noqa
-from .database_connection import load_predictions  # noqa
-from .database_connection import store_price_intervals  # noqa
-from .database_connection import load_total_grid_consumption_and_production  # noqa
-from .database_connection import store_predictions  # noqa
-from .database_connection import load_predictions  # noqa
-from .database_connection import load_cleared_trades  # noqa
-from .database_connection import load_grid_imbalance  # noqa
-from .database_connection import load_customer_prosumption  # noqa
-from .database_connection import get_current_game_id_and_timeslot  # noqa
+from .database_connection import create_db_connection_engine  # noqa
+from .database_connection import connect_to_local_database  # noqa
+from .database_connection import execute_sql_query  # noqa
+from .simulation_data import load_cleared_trades  # noqa
+from .simulation_data import load_grid_consumption_and_production  # noqa
+from .simulation_data import load_grid_imbalance  # noqa
+from .simulation_data import load_customer_prosumption  # noqa
+from .simulation_data import get_current_game_id_and_timeslot  # noqa
+from .predictions import load_predictions  # noqa
+from .predictions import store_price_intervals  # noqa
+from .predictions import store_predictions  # noqa
---
+++ src/ewiis3DatabaseConnector/predictions.py
@@ -0,0 +1,38 @@
+from ewiis3DatabaseConnector import execute_sql_query, connect_to_local_database, create_db_connection_engine
+
+
+def load_predictions(table_name, game_id):
+    try:
+        if game_id:
+            where_clause = ' WHERE game_id="{}"'.format(game_id)
+        else:
+            where_clause = ''
+
+        sql_statement = "SELECT * FROM {}{}".format(table_name, where_clause)
+        df_predictions = execute_sql_query(sql_statement)
+    except Exception as e:
+        print('Error occured while requesting `{}` table from db.'.format(table_name))
+        df_predictions = pd.DataFrame()
+    return df_predictions
+
+
+def store_predictions(df_prosumption_predictions, table_name):
+    cnx = create_db_connection_engine()
+    df_prosumption_predictions.to_sql(name=table_name, schema='ewiis3', con=cnx, if_exists='append', index=False)
+
+
+def store_price_intervals(df_intervals):
+    try:
+        conn = __connect_to_local_database()
+        conn.cursor()
+        cur = conn.cursor()
+        cur.execute("TRUNCATE TABLE ewiis3.wholesale_price_intervals")
+        conn.commit()
+        conn.close()
+
+        cnx = create_db_connection_engine()
+        df_intervals.to_sql(name='wholesale_price_intervals', schema='ewiis3', con=cnx, if_exists='append',
+                                          index=False)
+    except Exception as e:
+        print('Error occured during storing price intervals.')
+        print(e)
